5a78103d2cb6893fddf6c17a7bbac817dc773e7a
headstone <feuerschwanz1990@gmail.com>
2023-06-14 00:15:53 +0300

    checkedItems

--- CheckedItem.cs
+++ CheckedItem.cs
@@ -1,5 +1,6 @@
 ﻿namespace LilaChecker;
 
+// Определяем класс CheckedItem, который хранит информацию об URL и последнем полученном значении
 public class CheckedItem
 {
     public required string Url { get; init; }
